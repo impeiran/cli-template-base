1a887b868925accb0be4dfae8d173857d98f0f0e
chaser <1070045244@qq.com>
2019-09-05 22:07:44 +0800

    add: loader for fonts & medias

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -30,15 +30,6 @@ module.exports = {
         use: 'babel-loader'
       },
 
-      {
-        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-        loader: 'url-loader',
-        options: {
-          limit: 8192,
-          name: utils.assetsPath('assets/[name].[hash:7].[ext]')
-        }
-      },
-
       // sass loader
       {
         test: /\.(scss|sass)$/,
@@ -51,17 +42,32 @@ module.exports = {
         use: utils.genCssLoader()
       },
 
-      // media loader
-      // {
-      //   test: /\.(jpg|png|svg|gif)$/,
-      //   use: ['file-loader']
-      // },
-
-      // font loader
       {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ['file-loader']
-      }
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 8192,
+          name: utils.assetsPath('img/[name].[hash:7].[ext]')
+        }
+      },
+
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 8192,
+          name: utils.assetsPath('media/[name].[hash:7].[ext]')
+        }
+      },
+
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+        }
+      },
     ]
   },
 
